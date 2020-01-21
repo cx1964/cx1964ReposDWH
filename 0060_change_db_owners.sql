5a6277b1612ddb7434f16a1c9b93cd846597ff36
cx1964 <cx1964@mail.com>
2020-01-21 08:03:41 +0100

    wijzigen owner DWH databases

---
+++ 0060_change_db_owners.sql
@@ -0,0 +1,32 @@
+/* Filenaam   : 0060_change_db_owners.sql
+ * Functie    : SQL-T SQL2016
+ *              script tbv toewijzen van de applicatie database user
+ *              als owner van de database
+ * Opmerkingen: Dit script moet gedraaid worden nadat de lege database is gemaakt
+ *
+ * --------- history ---------------------
+ * v.0.0.0   2020-01-21   C.X. la Fontaine
+ */
+
+
+-- *** Begin Change owner TestSourceDB DB ***
+/* Selecteer de nieuwe lege applicatie database */ 
+use TestSourceDB
+go
+
+/* Maak de Applicatie database user eigenaar van de database */
+exec dbo.sp_changedbowner @loginame =  'myTestDBOwnerSource'
+go
+-- *** Einde Change owner TestSourceDB DB ***
+
+
+
+-- *** Begin Change owner TestIntegrationDB DB ***
+/* Selecteer de nieuwe lege applicatie database */ 
+use TestIntegrationDB
+go
+
+/* Maak de Applicatie database user eigenaar van de database */
+exec dbo.sp_changedbowner @loginame =  'myTestDBOwnerIntegration'
+go
+-- *** Einde Change owner TestIntegrationDB DB ***
