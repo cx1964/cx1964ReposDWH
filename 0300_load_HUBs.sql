5ef7a99edc1b30e780c960d6c6e137386495563f
cx1964 <cx1964@mail.com>
2020-01-22 10:00:26 +0100

    clorrect toevoegen H_organisatie_eenheid

--- 0300_load_HUBs.sql
+++ 0300_load_HUBs.sql
@@ -1,25 +1,39 @@
+-- Code obv documentatie zie
+-- [1]
+-- Buiding a scalable Data Warehouse 
+-- With Data Vault 2.0
+-- Daniel Linstedt
+-- Micheal Olschimke
+
+
 use [TestIntegrationDB]
 go
 
 insert into H_Organisatie_Eenheid
-select distinct
-                 HASHBYTES('SHA2_256', b.code) as [h_Organisatie_EenheidHashkey] -- SHA2_512 creeert een hash van 64 posities
-                ,b.code as code -- logische business key
-				,b.[meta_record_source]
-                ,b.[meta_load_date]     as [meta_load_date]
-			    ,b.[meta_create_time] as [meta_create_time]
-from [TestStagingDB].[dbo].[Organisatie_Eenheid] b -- nog gebruik maken van staging !!!!!!
-where
-       b.code NOT IN (  select code from [TestIntegrationDB].[dbo].[H_Organisatie_Eenheid] )
-	--   zie tekst paragraaf 12.1.1.1. DataVault 2.0 boek waarom een test op LoadDDate voor specifieke load datum.
-       --   Namelijk om staging (die meerdere laadjobs kan bevat), de meerdere loads in juiste volgorde te laden   
-            --  AND ( meta_load_date = ???? =  AND meta_create_time = ??? )     *** NOG AANZETTEN ***
- -- alternatief
- order by b.code
-         ,b.[meta_record_source]
-         ,b.[meta_load_date]           
-         ,b.[meta_create_time]
-
+select
+         HASHBYTES('SHA2_256', staging.code) as [H_Organisatie_EenheidHashkey] -- SHA2_512 creeert een hash van 64 posities
+        ,staging.code as nr -- logische business key
+        ,staging.[meta_record_source]
+        ,staging.[meta_load_date]
+        ,staging.[meta_create_time]
+from [TestStagingDB].[dbo].[Organisatie_Eenheid] staging
+inner join (
+             -- haal het eerste records uit staging
+             -- Alternatief dan genoemd in boek [1] paragraaf 12.1.1.1. tav verwerken meerdere sets in staging
+             select   [id]
+                     ,[code]
+                     ,min([meta_load_date]) as meta_load_date
+                     ,min([meta_create_time]) as meta_create_time
+             from [TestStagingDB].[dbo].[Organisatie_Eenheid]
+             group by id, clode -- business key
+           ) eerste
+		   on (     eerste.id = staging.id 
+		        and eerste.meta_load_date = staging.meta_load_date
+				and eerste.meta_create_time = staging.meta_create_time )
+where 1=1
+  -- voeg alleen nog niet bekende business keys toe  
+  and staging.[code] not in (select code 
+                           from [TestIntegrationDB].[dbo].[H_Medewerker])
 
 insert into H_Medewerker
 select
@@ -31,6 +45,7 @@ select
 from [TestStagingDB].[dbo].[Medewerker] staging
 inner join (
              -- haal het eerste records uit staging
+             -- Alternatief dan genoemd in boek [1] paragraaf 12.1.1.1. tav verwerken meerdere sets in staging
              select   [id]
                      ,[nr]
                      ,min([meta_load_date]) as meta_load_date
