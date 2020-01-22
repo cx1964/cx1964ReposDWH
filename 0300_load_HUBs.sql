8ab0600ce098f68f2ee3c6c0e81eabb5b66fca7f
cx1964 <cx1964@mail.com>
2020-01-22 09:49:28 +0100

    load correct h_medewerker

--- 0300_load_HUBs.sql
+++ 0300_load_HUBs.sql
@@ -22,20 +22,26 @@ where
 
 
 insert into H_Medewerker
-select distinct 
-                 HASHBYTES('SHA2_256', b.nr) as [h_MedewerkerHashkey] -- SHA2_512 creeert een hash van 64 posities
-                ,b.nr as nr -- logische business key
-				,b.[meta_record_source]
-                ,b.[meta_load_date]     as [meta_load_date]
-			    ,b.[meta_create_time] as [meta_create_time]
-from [TestStagingDB].[dbo].[Medewerker] b -- nog gebruik maken van staging !!!!!!
-where
-       b.nr NOT IN (  select nr from [TestIntegrationDB].[dbo].[H_Medewerker] )
-	--   zie tekst paragraaf 12.1.1.1. DataVault 2.0 boek waarom een test op LoadDDate voor specifieke load datum.
-       --   Namelijk om staging (die meerdere laadjobs kan bevat), de meerdere loads in juiste volgorde te laden   
-  --     AND ( meta_load_date = ???? =  AND meta_create_time = ??? )     *** NOG AANZETTEN ***
-   -- alternatief
- order by b.nr
-         ,b.[meta_record_source]
-         ,b.[meta_load_date]           
-         ,b.[meta_create_time]
+select
+         HASHBYTES('SHA2_256', staging.nr) as [h_MedewerkerHashkey] -- SHA2_512 creeert een hash van 64 posities
+        ,staging.nr as nr -- logische business key
+        ,staging.[meta_record_source]
+        ,staging.[meta_load_date]
+        ,staging.[meta_create_time]
+from [TestStagingDB].[dbo].[Medewerker] staging
+inner join (
+             -- haal het eerste records uit staging
+             select   [id]
+                     ,[nr]
+                     ,min([meta_load_date]) as meta_load_date
+                     ,min([meta_create_time]) as meta_create_time
+             from [TestStagingDB].[dbo].[Medewerker]
+             group by id, nr -- business key
+           ) eerste
+		   on (     eerste.id = staging.id 
+		        and eerste.meta_load_date = staging.meta_load_date
+				and eerste.meta_create_time = staging.meta_create_time )
+where 1=1
+  -- voeg alleen nog niet bekende business keys toe  
+  and staging.[nr] not in (select nr 
+                           from [TestIntegrationDB].[dbo].[H_Medewerker])
