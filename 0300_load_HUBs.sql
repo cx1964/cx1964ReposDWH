954efa931b528fcac9e7606e3fe29b978a1ceace
cx1964 <cx1964@mail.com>
2020-01-22 09:23:40 +0100

    eerste versie tbv toevoegen van HUBs

--- 0300_load_HUBs.sql
+++ 0300_load_HUBs.sql
@@ -2,30 +2,40 @@ use [TestIntegrationDB]
 go
 
 insert into H_Organisatie_Eenheid
-select distinct 
+select distinct
                  HASHBYTES('SHA2_256', b.code) as [h_Organisatie_EenheidHashkey] -- SHA2_512 creeert een hash van 64 posities
                 ,b.code as code -- logische business key
 				,b.[meta_record_source]
-                ,convert(date, getdate()) as [meta_load_date]
-			    ,convert(time, getdate()) as [meta_create_date]
+                ,b.[meta_load_date]     as [meta_load_date]
+			    ,b.[meta_create_time] as [meta_create_time]
 from [TestStagingDB].[dbo].[Organisatie_Eenheid] b -- nog gebruik maken van staging !!!!!!
 where
        b.code NOT IN (  select code from [TestIntegrationDB].[dbo].[H_Organisatie_Eenheid] )
 	--   zie tekst paragraaf 12.1.1.1. DataVault 2.0 boek waarom een test op LoadDDate voor specifieke load datum.
        --   Namelijk om staging (die meerdere laadjobs kan bevat), de meerdere loads in juiste volgorde te laden   
-  --     AND ( meta_load_date = ???? =  AND meta_create_time = ??? )     *** NOG AANZETTEN ***
+            --  AND ( meta_load_date = ???? =  AND meta_create_time = ??? )     *** NOG AANZETTEN ***
+ -- alternatief
+ order by b.code
+         ,b.[meta_record_source]
+         ,b.[meta_load_date]           
+         ,b.[meta_create_time]
 
 
 insert into H_Medewerker
 select distinct 
-                 HASHBYTES('SHA2_256', b.nr) as [h_Organisatie_EenheidHashkey] -- SHA2_512 creeert een hash van 64 posities
+                 HASHBYTES('SHA2_256', b.nr) as [h_MedewerkerHashkey] -- SHA2_512 creeert een hash van 64 posities
                 ,b.nr as nr -- logische business key
 				,b.[meta_record_source]
-                ,convert(date, getdate()) as [meta_load_date]
-			    ,convert(time, getdate()) as [meta_create_date]
+                ,b.[meta_load_date]     as [meta_load_date]
+			    ,b.[meta_create_time] as [meta_create_time]
 from [TestStagingDB].[dbo].[Medewerker] b -- nog gebruik maken van staging !!!!!!
 where
-       b.nr NOT IN (  select nr from [TestIntegrationDB].[dbo].[H_Organisatie_Eenheid] )
+       b.nr NOT IN (  select nr from [TestIntegrationDB].[dbo].[H_Medewerker] )
 	--   zie tekst paragraaf 12.1.1.1. DataVault 2.0 boek waarom een test op LoadDDate voor specifieke load datum.
        --   Namelijk om staging (die meerdere laadjobs kan bevat), de meerdere loads in juiste volgorde te laden   
   --     AND ( meta_load_date = ???? =  AND meta_create_time = ??? )     *** NOG AANZETTEN ***
+   -- alternatief
+ order by b.nr
+         ,b.[meta_record_source]
+         ,b.[meta_load_date]           
+         ,b.[meta_create_time]
