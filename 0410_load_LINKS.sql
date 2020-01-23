ec491b747ba5ac37daf23fdceda3b629052f25f4
cx1964 <cx1964@mail.com>
2020-01-23 09:51:44 +0100

    concept laden LINKs -- lees 1e set uit staging

--- 0410_load_LINKS.sql
+++ 0410_load_LINKS.sql
@@ -4,7 +4,7 @@ use [TestIntegrationDB]
 go
 
 
-select  HASHBYTES('SHA2_256', sm.nr+so.code) as [L_Medewerker_Organisatie_EenheidHashkey] -- nog geode hashkey bepalen voor LINK key; nu nog niet uniek
+select  HASHBYTES('SHA2_256', str(sm.nr)+str(so.code)) as [L_Medewerker_Organisatie_EenheidHashkey] -- nog goede hashkey bepalen voor LINK key; nu nog niet uniek
        ,HASHBYTES('SHA2_256', sm.nr) as [H_MedewerkerHashkey]
 	   ,HASHBYTES('SHA2_256', so.code) as [h_Organisatie_EenheidHashkey]
 	   ,concat(str(sm.id,8,0),str(so.id,8,0))+convert(varchar(24), getdate(), 121) -- test tbv hash key maken
@@ -28,3 +28,41 @@ where not exists
     and l.[H_MedewerkerHashkey] = hm.[H_MedewerkerHashkey]
     and l.[H_Organisatie_EenheidHashkey] = ho.[h_Organisatie_EenheidHashkey]
 )
+
+
+-- todo
+-- onderstaande afmaken
+-- als klaar kan query hierboven weg.
+-- todo:
+--       1. hashkey uit h_org_eenheid toevoegen
+--       2. haskkey voor link maken op concat buesiness keys
+--       3. na denken over hoe link bij werken als fk wijzigd in bronsysteem of fk leeg wordt
+
+select distinct
+                eerste.id
+               ,eerste.nr
+			   ,mh.H_MedewerkerHashkey
+               ,os.id
+			   ,os.code
+			   ,m.meta_record_source
+               ,eerste.meta_load_date
+	           ,eerste.meta_create_time
+from (
+
+             -- Selecteer alleen records uit staging met de oudste datum 
+			 -- Filter op tabel met FK dwz, hier Medewerker 
+             select   ms.[id] as id -- primary key
+                     ,ms.[nr] as nr -- business key
+                     ,min(ms.[meta_load_date])   as meta_load_date
+                     ,min(ms.[meta_create_time]) as meta_create_time
+             from [TestStagingDB].[dbo].[Medewerker] ms -- tabel met fk
+             group by ms.id, ms.nr -- business key 
+	 ) eerste
+inner join [TestStagingDB].[dbo].[Medewerker] m
+      on m.id = eerste.id
+inner join [TestStagingDB].[dbo].[Organisatie_Eenheid] os -- parent tabel
+      on os.id = m.werkt_voor_org_eenheid
+-- joins tbv ophalen hashkeys van de hubs
+-- tbv hashkey medewerker
+inner join	[TestIntegrationDB].[dbo].[H_medewerker] mh
+       on mh.nr = eerste.nr
