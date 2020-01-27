be6ec80b3be2b301c272e300bf81b0adf46b1fb5
cx1964 <cx1964@mail.com>
2020-01-27 14:35:47 +0100

    concept versie load s_medewerker

--- 0420_load_SATs.sql
+++ 0420_load_SATs.sql
@@ -1,57 +1,78 @@
 -- vullen sat S_Medewerker en S_Organisatie_Eenheid
 
+-- Artikel over merge statement
+-- https://www.sqlservertutorial.net/sql-server-basics/sql-server-merge/
+
+
 use [TestIntegrationDB]
 go
 
---insert into L_Medewerker_Organisatie_Eenheid 
---select distinct
---                HASHBYTES('SHA2_256', str(eerste.nr)+str(os.code)) as [L_Medewerker_Organisatie_EenheidHashkey]
---                --  ,eerste.id
---                --  ,eerste.nr
---			   ,mh.H_MedewerkerHashkey
---               -- ,os.id
---			   -- ,os.code
---			   ,oh.H_Organisatie_EenheidHashkey
---			   ,m.meta_record_source
---               ,eerste.meta_load_date
---	           ,eerste.meta_create_time
---from (
+MERGE [TestIntegrationDB].[dbo].[S_Medewerker] sm
+USING [TestStagingDB].[dbo].[Medewerker] stgm
+ON ( sm.H_MedewerkerHashkey =  HASHBYTES('SHA2_256', str(stgm.nr) ) ) -- hash van de business key
+WHEN MATCHED
+    -- update actie 
+    THEN 
+	     update
+	     set 
+             sm.voorletters=stgm.voorletters
+            ,sm.voorvoegsel=stgm.voorvoegsel
+		    ,sm.achternaam=stgm.achternaam
+		    ,sm.geboortedatum=stgm.geboortedatum
+		    ,sm.aow_datum=stgm.aow_datum
+WHEN NOT MATCHED BY TARGET
+     -- insert actie
+    THEN
+	    insert (
+                 [H_MedewerkerHashkey]
+                ,[voorletters]
+                ,[voorvoegsel]
+                ,[achternaam]
+                ,[geboortedatum]
+                ,[aow_datum]
+                ,[meta_record_source]
+                ,[meta_load_date]
+                ,[meta_create_time]		
+		) values(
+		   HASHBYTES('SHA2_256', str(stgm.nr))
+		  ,stgm.[voorletters]
+		  ,stgm.[voorvoegsel]
+          ,stgm.[achternaam]
+          ,stgm.[geboortedatum]
+          ,stgm.[aow_datum]
+          ,stgm.[meta_record_source]
+          ,stgm.[meta_load_date]
+          ,stgm.[meta_create_time]	
+		)	
+-- geen delete actie
+-- als delete actie dcan moet dit niet vergelijken sat met staging maar sat met bron
+--WHEN NOT MATCHED BY SOURCE
+--    -- delete actie
+--    THEN 
+--	     NULL
+;
 
---             -- Selecteer alleen records uit staging met de oudste datum 
---			 -- Filter op tabel met FK dwz, hier Medewerker 
---             select   ms.[id] as id -- primary key
---                     ,ms.[nr] as nr -- business key
---                     ,min(ms.[meta_load_date])   as meta_load_date
---                     ,min(ms.[meta_create_time]) as meta_create_time
---             from [TestStagingDB].[dbo].[Medewerker] ms -- tabel met fk
---             group by ms.id, ms.nr -- business key 
---	 ) eerste
---inner join [TestStagingDB].[dbo].[Medewerker] m
---      on m.id = eerste.id
---inner join [TestStagingDB].[dbo].[Organisatie_Eenheid] os -- parent tabel
---      on os.id = m.werkt_voor_org_eenheid
----- joins tbv ophalen hashkeys van de hubs
----- tbv hashkey Medewerker
---inner join	[TestIntegrationDB].[dbo].[H_medewerker] mh
---       on mh.nr = eerste.nr
----- tbv hashkey Organisatie_Eenheid
---inner join	[TestIntegrationDB].[dbo].[H_Organisatie_Eenheid] oh
---       on oh.code = os.code
----- check alleen records toevoegen in link tabel die nog niet bekend zijn
---and not exists
---(
---   select 'dummy'
---   from L_Medewerker_Organisatie_Eenheid lmo
---    where 1=1
---	  and lmo.H_MedewerkerHashkey = mh.H_MedewerkerHashkey
---	  and lmo.H_Organisatie_EenheidHashkey = oh.H_Organisatie_EenheidHashkey
---)
 
 
-select *
-from [TestStagingDB].[dbo].[Medewerker] sm
--- voer wijziging door obv volgorde van datum en tijd in staging (oudste eerste, nieuweste als laast)
-order by 
-          sm.nr asc  -- business key
-         ,sm.meta_load_date asc
-		 ,sm.meta_create_time asc
+-- oud
+--select *
+--from [TestStagingDB].[dbo].[Medewerker] stgm
+---- voer wijziging door obv volgorde van datum en tijd in staging (oudste eerste, nieuweste als laast)
+--left outer join [TestIntegrationDB].[dbo].[S_Medewerker] sm
+--     on (     sm.H_MedewerkerHashkey =  HASHBYTES('SHA2_256', str(stgm.nr))
+--	      AND sm.meta_load_date   IS NULL
+--		  AND sm.meta_create_time IS NULL
+--         )
+--where 1=1
+--  and (
+--         -- change detection
+--            ISNULL(stgm.voorletters, '')   != ISNULL(sm.voorletters, '')
+--         OR ISNULL(stgm.voorvoegsel, '')   != ISNULL(sm.voorvoegsel, '')
+--		 OR ISNULL(stgm.achternaam, '')    != ISNULL(sm.achternaam, '')
+--		 OR ISNULL(stgm.geboortedatum, '') != ISNULL(sm.geboortedatum, '')
+--		 OR ISNULL(stgm.aow_datum, '')     != ISNULL(sm.aow_datum, '')
+--      )
+--order by 
+--          stgm.meta_load_date asc
+--		 ,stgm.meta_create_time asc
+--		 ,stgm.nr asc  -- business key
