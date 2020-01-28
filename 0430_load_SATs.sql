796918cef59571f10449cec143aa094d40a23255
cx1964 <cx1964@mail.com>
2020-01-28 14:12:05 +0100

    reshuffle files

---
+++ 0430_load_SATs.sql
@@ -0,0 +1,12 @@
+-- vullen sat S_Medewerker en S_Organisatie_Eenheid
+
+-- Artikel over merge statement
+-- https://www.sqlservertutorial.net/sql-server-basics/sql-server-merge/
+
+
+use [TestIntegrationDB]
+go
+
+-- run procedure to load S_Medewerker
+exec Load_S_Medewerker
+go
