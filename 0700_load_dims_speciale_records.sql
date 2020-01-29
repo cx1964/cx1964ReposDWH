e964ba533eeabe41164bb78027286ffeee236dbc
cx1964 <cx1964@mail.com>
2020-01-29 14:11:51 +0100

    script tbv load special dim records

--- 0700_load_dims_speciale_records.sql
+++ 0700_load_dims_speciale_records.sql
@@ -15,14 +15,22 @@
 -- -1 = Niet gevonden
 -- -2 = Onbekend
 
-use [TestIntegrationDB]
+use [TestPresentationDB]
 go
 
-insert into Dim_Medewerker (organisatie_eenheid_key,code,naam,meta_record_source,meta_load_date,meta_create_time) values (-1, 'Niet gevonden', NULL, 'system', convert(date, getdate()) , convert(time, getdate()));
-insert into Dim_Medewerker (organisatie_eenheid_key,code,naam,meta_record_source,meta_load_date,meta_create_time) values (-2, 'Onbekend', NULL, 'system', convert(date, getdate()) , convert(time, getdate()));
+SET IDENTITY_INSERT dbo.Dim_Organisatie_Eenheid ON;  
+go 
+insert into Dim_Organisatie_Eenheid (organisatie_eenheid_key,code,naam,meta_record_source,meta_load_date,meta_create_time) values (-1, 'Niet gevonden', NULL, 'system', convert(date, getdate()) , convert(time, getdate()));
+insert into Dim_Organisatie_Eenheid (organisatie_eenheid_key,code,naam,meta_record_source,meta_load_date,meta_create_time) values (-2, 'Onbekend', NULL, 'system', convert(date, getdate()) , convert(time, getdate()));
 go
+SET IDENTITY_INSERT dbo.Dim_Organisatie_Eenheid OFF;  
+go  
 
 
-insert into Dim_Organisatie_Eenheid (medewerker_key,nr,voorletters,voorvoegsel,achternaam,geboortedatum,aow_datum,meta_record_source,meta_load_date,meta_create_time) values (-1,NULL,NULL,NULL,'Niet gevonden','99991231','99991231','system', convert(date, getdate()) , convert(time, getdate()));
-insert into Dim_Organisatie_Eenheid (medewerker_key,nr,voorletters,voorvoegsel,achternaam,geboortedatum,aow_datum,meta_record_source,meta_load_date,meta_create_time) values (-1,NULL,NULL,NULL,'Onbekend','99991231','99991231','system', convert(date, getdate()) , convert(time, getdate()));
+SET IDENTITY_INSERT dbo.Dim_Medewerker ON;  
+go  
+insert into Dim_Medewerker (medewerker_key,nr,voorletters,voorvoegsel,achternaam,geboortedatum,aow_datum,meta_record_source,meta_load_date,meta_create_time) values (-1,'??????',NULL,NULL,'Niet gevonden','99991231','99991231','system', convert(date, getdate()) , convert(time, getdate()));
+insert into Dim_Medewerker (medewerker_key,nr,voorletters,voorvoegsel,achternaam,geboortedatum,aow_datum,meta_record_source,meta_load_date,meta_create_time) values (-2,'??????',NULL,NULL,'Onbekend','99991231','99991231','system', convert(date, getdate()) , convert(time, getdate()));
 go
+SET IDENTITY_INSERT dbo.Dim_Medewerker OFF;  
+go  
