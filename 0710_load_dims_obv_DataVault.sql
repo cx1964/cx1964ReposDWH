280ce380f31f988ccb1580a907b7f03e9328b123
cx1964 <cx1964@mail.com>
2020-01-30 09:20:00 +0100

    reshuffle + Laden Dim_Organisatie_Eenheid

---
+++ 0710_load_dims_obv_DataVault.sql
@@ -0,0 +1,68 @@
+-- vullen Dims
+
+use [TestPresentationDB]
+go
+
+-- insert into Dim_Organisatie_eenheid
+SET IDENTITY_INSERT dbo.Dim_Organisatie_eenheid OFF;  
+go
+insert into [Dim_Organisatie_Eenheid]
+(
+      -- Business key 
+       [code]
+      -- Properties
+      ,[naam]
+	  ,[meta_record_source]
+	  ,[meta_load_date]
+	  ,[meta_create_time]
+)
+select 
+        -- Business key 
+         h.code
+        -- Properties
+		,s.naam
+		-- Meta data
+		,h.meta_record_source
+		,h.meta_load_date
+		,h.meta_create_time
+from [TestIntegrationDB].[dbo].[H_Organisatie_Eenheid] h
+inner join [TestIntegrationDB].[dbo].[S_Organisatie_Eenheid] s
+      on s.H_Organisatie_EenheidHashkey = h.h_Organisatie_EenheidHashkey
+go
+
+-- OK
+--insert into Dim_Medewerker
+SET IDENTITY_INSERT dbo.Dim_Medewerker OFF;  
+go  
+insert into [Dim_Medewerker]
+(
+      -- Business key 
+       [nr]
+      -- Properties
+      ,[voorletters]
+      ,[voorvoegsel]
+      ,[achternaam]
+      ,[geboortedatum]
+      ,[aow_datum]
+	  -- Meta data
+	  ,[meta_record_source]
+	  ,[meta_load_date]
+	  ,[meta_create_time]
+)
+select 
+         -- Business key
+         h.nr
+		 -- Properties
+	    ,s.voorletters
+        ,s.voorvoegsel
+        ,s.achternaam
+        ,s.geboortedatum
+        ,s.aow_datum
+		-- Meta data
+		,h.meta_record_source
+		,h.meta_load_date
+		,h.meta_create_time
+from [TestIntegrationDB].[dbo].[H_Medewerker] h
+inner join [TestIntegrationDB].[dbo].[S_Medewerker] s
+      on s.H_MedewerkerHashkey = h.H_MedewerkerHashkey 
+go
