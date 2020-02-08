0c101b28cc609912fc5b70732f78cb6e4b8b74b2
cx1964 <cx1964@mail.com>
2020-02-08 11:30:16 +0100

    list db filenames

--- Docker_Linux_tbv_SQLServer/Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/9000_list_sql_dbs.sh
+++ Docker_Linux_tbv_SQLServer/Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/9000_list_sql_dbs.sh
@@ -1,4 +1,7 @@
 # https://docs.microsoft.com/en-us/sql/linux/quickstart-install-connect-docker?view=sql-server-ver15&pivots=cs1-bash
 
-# create SQL server database owners for DWH databases
+# List SQL server databases in Docker container
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Welkom01" -Q "select database_id, name, create_date from sys.databases"
+
+# List used files form databases
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Welkom01" -Q "select name, physical_name from sys.master_files order by 1"
