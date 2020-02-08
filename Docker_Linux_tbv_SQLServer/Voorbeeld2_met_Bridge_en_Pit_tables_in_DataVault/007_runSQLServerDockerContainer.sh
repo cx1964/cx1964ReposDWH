84d2100a79e8b6253d2770e88a6f9f0f020d369f
cx1964 <cx1964@mail.com>
2020-02-08 10:52:41 +0100

    experiment db files en docker volume

--- Docker_Linux_tbv_SQLServer/Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/007_runSQLServerDockerContainer.sh
+++ Docker_Linux_tbv_SQLServer/Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/007_runSQLServerDockerContainer.sh
@@ -12,9 +12,13 @@ docker run \
        --name 'sql19' \
        -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD='$PASSWORD \
        -p 1433:1433 \
-       -v sqldata1:/var/opt/mssql \
+       -v mssqlserverdata1:/var/opt/mssql \
        -d mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
 
+# -v sqldata1:/var/opt/mssql
+
+# check container
+docker ps -a
 
 # use DBeaver to test connection
 # https://dbeaver.io/    
