495f2debbfb296a1079ba228ab124862e1a5b3b6
cx1964 <cx1964@mail.com>
2020-02-09 15:47:37 +0100

    Load Hub tables met hashkey uit staging

---
+++ Docker_Linux_tbv_SQLServer/Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0400_load_HUBs.sh
@@ -0,0 +1,4 @@
+# https://docs.microsoft.com/en-us/sql/linux/quickstart-install-connect-docker?view=sql-server-ver15&pivots=cs1-bash
+
+# create SQL server database owners for DWH databases
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Welkom01" -i "../../Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0400_load_HUBs.sql" -o "./0400_load_HUBs.log"
--- Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0400_load_HUBs.sql
+++ Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0400_load_HUBs.sql
@@ -8,12 +8,9 @@
 use [TestIntegrationDB2]
 go
 
--- *** Nog aanpassen hashkeys uit staging halen
-
 insert into H_Organisatie_Eenheid2
 select
-         -- letop hashkeys uit staging halen ipv zelf genereren
-         HASHBYTES('SHA2_256', staging.code) as [H_Organisatie_EenheidHashkey] -- SHA2_512 creeert een hash van 64 posities
+         staging.hashkey as [H_Organisatie_EenheidHashkey] -- SHA2_512 creeert een hash van 64 posities
         ,staging.code as nr -- logische business key
         ,staging.[meta_record_source]
         ,staging.[meta_load_date]
@@ -31,21 +28,16 @@ inner join (
            ) eerste
 		   on (     eerste.id = staging.id 
 		        and eerste.meta_load_date = staging.meta_load_date
-				and eerste.meta_create_time = staging.meta_create_time )
+		        and eerste.meta_create_time = staging.meta_create_time )
 where 1=1
   -- voeg alleen nog niet bekende business keys toe  
   and staging.[code] not in (select code 
-                           from [TestIntegrationDB2].[dbo].[H_Organisatie_Eenheid2])
+                             from [TestIntegrationDB2].[dbo].[H_Organisatie_Eenheid2])
 
-
-
-
--- *** Nog aanpassen hashkeys uit staging halen
 
 insert into H_Medewerker2
 select
-         -- letop hashkeys uit staging halen ipv zelf genereren
-         HASHBYTES('SHA2_256', staging.nr) as [h_MedewerkerHashkey] -- SHA2_512 creeert een hash van 64 posities
+         staging.hashkey  as [h_MedewerkerHashkey] -- SHA2_512 creeert een hash van 64 posities
         ,staging.nr as nr -- logische business key
         ,staging.[meta_record_source]
         ,staging.[meta_load_date]
@@ -63,8 +55,16 @@ inner join (
            ) eerste
 		   on (     eerste.id = staging.id 
 		        and eerste.meta_load_date = staging.meta_load_date
-				and eerste.meta_create_time = staging.meta_create_time )
+		        and eerste.meta_create_time = staging.meta_create_time )
 where 1=1
   -- voeg alleen nog niet bekende business keys toe  
   and staging.[nr] not in (select nr 
                            from [TestIntegrationDB2].[dbo].[H_Medewerker2])
+
+
+-- Debug info
+select *
+from dbo.H_Medewerker2 
+
+select *
+from dbo.H_Organisatie_Eenheid2
