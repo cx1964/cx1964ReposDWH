9e09c4a0b6c253a97f0c20e8309e29017048e9da
cx1964 <cx1964@mail.com>
2020-02-12 16:11:39 +0100

    concept laden DIMs+FACTs voorbeeld2;niet foutloos

---
+++ Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0700_load_dims_speciale_records_traditioneel.sql
@@ -0,0 +1,46 @@
+-- Vul de DIMs met de speciale records.
+-- De speciale records onstaan, dat men alle velden van de FACTs uit verplichte velden (dus zonder NULL values)
+-- wil hebben, omdat NULL values niet goed samengaan met indexen.
+-- Daarom heeft men een speciale of meerdere speciale waarden nodig om aan te gegeven,
+-- dat iets geen waarde heeft.
+-- Aangezien de NULL value voor meerdere betekenissen wordt gebruikt, namelijk:
+--    1. onbekend
+--    2. geen waarde
+--    enz.
+--  
+-- De waarden en het aantal records voor de speciale gevallen zijn niet standaard
+-- Hierin kan men zelf keuzes maken.
+
+-- Ik gebruik de methode van oude DWH
+-- -1 = Niet gevonden
+-- -2 = Onbekend
+
+use [TestPresentationDB2]
+go
+
+-- zet fk uit tbv laden
+ALTER TABLE [dbo].[FACT_gepensioneerde_per_OE]
+NOCHECK CONSTRAINT FK_FACT_gepensioneerde_per_OE_Dim_Organisatie_Eenheid;  
+GO  
+
+truncate table Dim_Medewerker
+truncate table Dim_Organisatie_Eenheid -- gaat fout ???  of moet fk droppen?
+-- melding: Msg 4712, Level 16, State 1, Line 27
+-- Cannot truncate table 'Dim_Organisatie_Eenheid' because it is being referenced by a FOREIGN KEY constraint.
+
+SET IDENTITY_INSERT dbo.Dim_Organisatie_Eenheid ON;  
+go 
+insert into Dim_Organisatie_Eenheid (organisatie_eenheid_key,code,naam,meta_record_source,meta_load_date,meta_create_time) values (-1, 'Niet gevonden', NULL, 'system', convert(date, getdate()) , convert(time, getdate()));
+insert into Dim_Organisatie_Eenheid (organisatie_eenheid_key,code,naam,meta_record_source,meta_load_date,meta_create_time) values (-2, 'Onbekend', NULL, 'system', convert(date, getdate()) , convert(time, getdate()));
+go
+SET IDENTITY_INSERT dbo.Dim_Organisatie_Eenheid OFF;  
+go  
+
+
+SET IDENTITY_INSERT dbo.Dim_Medewerker ON;  
+go  
+insert into Dim_Medewerker (medewerker_key,nr,voorletters,voorvoegsel,achternaam,geboortedatum,aow_datum,meta_record_source,meta_load_date,meta_create_time) values (-1,'??????',NULL,NULL,'Niet gevonden','99991231','99991231','system', convert(date, getdate()) , convert(time, getdate()));
+insert into Dim_Medewerker (medewerker_key,nr,voorletters,voorvoegsel,achternaam,geboortedatum,aow_datum,meta_record_source,meta_load_date,meta_create_time) values (-2,'??????',NULL,NULL,'Onbekend','99991231','99991231','system', convert(date, getdate()) , convert(time, getdate()));
+go
+SET IDENTITY_INSERT dbo.Dim_Medewerker OFF;  
+go  
