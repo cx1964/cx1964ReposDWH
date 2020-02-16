cf5cf9a930ab37e6ff389089b98c02dc25e7445b
cx1964 <cx1964@mail.com>
2020-02-16 15:44:01 +0100

    load fact method1 klaar zonder lookup;nog drop fk

--- Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0720_load_facts_obv_DataVault_traditioneel.sql
+++ Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0720_load_facts_obv_DataVault_traditioneel.sql
@@ -1,5 +1,8 @@
 -- vullen Facts obv tranditionele manier (zonder bridge table)
 
+-- Methode1 obv Hashkey zonder Look-up naar HUB en DIM
+-- in commentaar Methode2 obv fk van facts middels Look-up naar HUB en DIM
+
 use [TestPresentationDB2]
 go
 
@@ -14,53 +17,52 @@ truncate table [dbo].[FACT_gepensioneerde_per_OE]
 -- waarbij de surrogate keys de hashkeys van de HUBs zijn uit de DataVault en
 -- de foreign keys in de FACTs verwijzen naar deze Hhash keys van de DIMs
 
--- ToDo
--- *** Eerst alleen nog het select commando zonder insert ***
--- *** Nog aanpassen Definitie fact tabel ***
--- *** test of query methode 1 en methode 2evenveel records opleveren
-
--- *** insert into [dbo].[FACT_gepensioneerde_per_OE] **
+insert into [dbo].[FACT_gepensioneerde_per_OE]
 -- Hoogste niveau select tbv om meta hub gegevens te joinen aan resultaat
 select
-        -- Keys naar DIMs
-        -- oud obv lookup op DIM do.[organisatie_eenheid_key]         as [Organisatie_Eenheid_key]
-        -- nieuw
-        resultaat.H_Organisatie_Eenheid2Hashkey    as [H_Organisatie2HashKey]
-             
-		
-		-- Measures
-       ,resultaat.aantal_gepensioneerden     as [aantal_gepensioneerden]
-	   
-	   -- Meta data ( waarvandaan uit LINK of SAT??????????????????????????????)
-	   ,rh.meta_record_source                as [meta_record_source]
-	   ,rh.meta_load_date                    as [meta_load_date]
-	   ,rh.meta_create_time                  as [meta_create_time]
+          -- *** Keys naar DIMs ***
+          -- oud obv lookup op DIM do.[organisatie_eenheid_key]         as [Organisatie_Eenheid_key]
+ 
+          -- nieuw
+          -- fk naar surrogate key van de Dim_Organisatie_Eenheid2 obv hashkeys
+          resultaat.H_Organisatie_Eenheid2Hashkey    as [H_Organisatie2HashKey]
+          
+         -- *** Measures ***
+         ,resultaat.aantal_gepensioneerden           as [aantal_gepensioneerden]
+          
+         -- *** Meta data ***
+         ,resultaat.meta_record_source               as [meta_record_source]
+         ,resultaat.meta_load_date                   as [meta_load_date]
+         ,resultaat.meta_create_time                 as [meta_create_time]
 from (
   -- tel alle aantallen op als resultaat
   select  
           data.h_Organisatie_Eenheid2Hashkey  as h_Organisatie_Eenheid2Hashkey
          ,sum(data.aantal)                    as aantal_gepensioneerden
-
+         -- neem vanwege de group by de max alle Meta gegevens van de SATs
+         ,max(data.meta_record_source)        as meta_record_source 
+         ,max(data.meta_load_date)            as meta_load_date
+	   ,max(data.meta_create_time)          as meta_create_time
   from (
     -- bepaal per organisatie eenheid medewerkers met aow_datum > geboortedatum
-
-    -- ToDo 
-    --       onderstaande query is nu goed
-    --       Nog Meta data toevoegen aan het hogere niveau, en dan is query klaar
-    --       Gebruik geen look ups naar LINKs
     select 
               l.H_Organisatie_Eenheid2Hashkey 
              ,sm.aow_datum  
-	       ,sm.geboortedatum
-	       ,1 as aantal  
+             ,sm.geboortedatum
+             ,1 as aantal
+             -- Meta gegevens uit SAT
+             ,sm.meta_record_source
+             ,sm.meta_load_date
+	       ,sm.meta_create_time
     from [TestIntegrationDB2].[dbo].[L_Medewerker2_Organisatie_Eenheid2] l
     inner join [TestIntegrationDB2].[dbo].[S_Medewerker2_vrtrw] sm
           on sm.H_Medewerker2Hashkey = l.H_Medewerker2Hashkey
     where sm.aow_datum > sm.geboortedatum
-    ) data
+  ) data
     --group by data.code, data.h_Organisatie_EenheidHashkey
     group by data.H_Organisatie_Eenheid2Hashkey
 ) resultaat
+-- niet meer nodig: Look-ups naar HUB en DIM
 -- niet meer nodig: inner join  [TestIntegrationDB].[dbo].[H_Organisatie_Eenheid] rh
 -- niet meer nodig:       on rh.h_Organisatie_EenheidHashkey = resultaat.h_Organisatie_EenheidHashkey
 -- niet meer nodig: inner join [TestPresentationDB].[dbo].[Dim_Organisatie_Eenheid] do
