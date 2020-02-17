f4d820541bc4b3be8c5aeaa4f660ae2bfb31239c
cx1964 <cx1964@mail.com>
2020-02-17 07:43:49 +0100

    Obv old SAT data => gebr min() + comment Methode2

--- Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0720_load_facts_obv_DataVault_traditioneel.sql
+++ Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0720_load_facts_obv_DataVault_traditioneel.sql
@@ -39,10 +39,11 @@ from (
   select  
           data.h_Organisatie_Eenheid2Hashkey  as h_Organisatie_Eenheid2Hashkey
          ,sum(data.aantal)                    as aantal_gepensioneerden
-         -- neem vanwege de group by de max alle Meta gegevens van de SATs
-         ,max(data.meta_record_source)        as meta_record_source 
-         ,max(data.meta_load_date)            as meta_load_date
-	   ,max(data.meta_create_time)          as meta_create_time
+         -- neem vanwege de group by de min alle Meta gegevens van de SATs
+		 -- gebruik de min zodat altijd de oudste gegevens worden genomen uit de SATs
+         ,min(data.meta_record_source)        as meta_record_source 
+         ,min(data.meta_load_date)            as meta_load_date
+	     ,min(data.meta_create_time)          as meta_create_time
   from (
     -- bepaal per organisatie eenheid medewerkers met aow_datum > geboortedatum
     select 
@@ -53,7 +54,7 @@ from (
              -- Meta gegevens uit SAT
              ,sm.meta_record_source
              ,sm.meta_load_date
-	       ,sm.meta_create_time
+	         ,sm.meta_create_time
     from [TestIntegrationDB2].[dbo].[L_Medewerker2_Organisatie_Eenheid2] l
     inner join [TestIntegrationDB2].[dbo].[S_Medewerker2_vrtrw] sm
           on sm.H_Medewerker2Hashkey = l.H_Medewerker2Hashkey
@@ -74,53 +75,52 @@ from (
 -- Tbv afhankelijk laden, dwz eerste DIMs en dan FACTs laden 
 -- Dwz niet parallel laden DataMart
 -- Onderstaande query werkt als de DIMs als PK hebben die bestaat uit een IDENTITY column
+--
+--insert into [dbo].[FACT_gepensioneerde_per_OE]
+---- Hoogste niveau select tbv om meta hub gegevens te joinen aan resultaat
+--select
+--        -- Keys naar DIMs
+--        do.[organisatie_eenheid_key]         as [Organisatie_Eenheid_key]
 
-insert into [dbo].[FACT_gepensioneerde_per_OE]
--- Hoogste niveau select tbv om meta hub gegevens te joinen aan resultaat
-select
-        -- Keys naar DIMs
-        do.[organisatie_eenheid_key]         as [Organisatie_Eenheid_key]
-
-        -- debug:  resultaat.code
+--        -- debug:  resultaat.code
 		
-		-- Measures
-       ,resultaat.aantal_gepensioneerden     as [aantal_gepensioneerden]
+--		-- Measures
+--       ,resultaat.aantal_gepensioneerden     as [aantal_gepensioneerden]
 	   
-	   -- Meta data
-	   ,rh.meta_record_source                as [meta_record_source]
-	   ,rh.meta_load_date                    as [meta_load_date]
-	   ,rh.meta_create_time                  as [meta_create_time]
-from (
-  -- tel alle aantallen op als resultaat
-  select  
-          data.h_Organisatie_EenheidHashkey
-  	  	 ,data.code                           as  code
-         ,sum(data.aantal)                    as aantal_gepensioneerden
-
-  from (
-    -- bepaal per organisatie eenheid medewerkers met aow_datum > geboortedatum
-    select 
-            ho.h_Organisatie_EenheidHashkey
-           ,ho.code  
-           ,sm.aow_datum  
-	       ,sm.geboortedatum
-	       ,1 as aantal  
-    from [TestIntegrationDB].[dbo].[L_Medewerker_Organisatie_Eenheid] l
-    inner join  [TestIntegrationDB].[dbo].[H_Organisatie_Eenheid] ho
-          on ho.h_Organisatie_EenheidHashkey = l.H_Organisatie_EenheidHashkey
-    inner join [TestIntegrationDB].[dbo].[H_Medewerker] hm
-          on hm.H_MedewerkerHashkey = l.H_MedewerkerHashkey
-    inner join [TestIntegrationDB].[dbo].[S_Medewerker] sm
-          on sm.H_MedewerkerHashkey = hm.H_MedewerkerHashkey
-    where sm.aow_datum > sm.geboortedatum
-    ) data
-    group by data.code, data.h_Organisatie_EenheidHashkey
-) resultaat
--- 2 joins tbv Lookup naar DIM
-inner join  [TestIntegrationDB].[dbo].[H_Organisatie_Eenheid] rh
-      on rh.h_Organisatie_EenheidHashkey = resultaat.h_Organisatie_EenheidHashkey
-inner join [TestPresentationDB].[dbo].[Dim_Organisatie_Eenheid] do
-      on do.code = resultaat.code 
+--	   -- Meta data
+--	   ,rh.meta_record_source                as [meta_record_source]
+--	   ,rh.meta_load_date                    as [meta_load_date]
+--	   ,rh.meta_create_time                  as [meta_create_time]
+--from (
+--  -- tel alle aantallen op als resultaat
+--  select  
+--          data.h_Organisatie_EenheidHashkey
+--  	  	 ,data.code                           as  code
+--         ,sum(data.aantal)                    as aantal_gepensioneerden
+--  from (
+--    -- bepaal per organisatie eenheid medewerkers met aow_datum > geboortedatum
+--    select 
+--            ho.h_Organisatie_EenheidHashkey
+--           ,ho.code  
+--           ,sm.aow_datum  
+--	       ,sm.geboortedatum
+--	       ,1 as aantal  
+--    from [TestIntegrationDB].[dbo].[L_Medewerker_Organisatie_Eenheid] l
+--    inner join  [TestIntegrationDB].[dbo].[H_Organisatie_Eenheid] ho
+--          on ho.h_Organisatie_EenheidHashkey = l.H_Organisatie_EenheidHashkey
+--    inner join [TestIntegrationDB].[dbo].[H_Medewerker] hm
+--          on hm.H_MedewerkerHashkey = l.H_MedewerkerHashkey
+--    inner join [TestIntegrationDB].[dbo].[S_Medewerker] sm
+--          on sm.H_MedewerkerHashkey = hm.H_MedewerkerHashkey
+--    where sm.aow_datum > sm.geboortedatum
+--    ) data
+--    group by data.code, data.h_Organisatie_EenheidHashkey
+--) resultaat
+---- 2 joins tbv Lookup naar DIM
+--inner join  [TestIntegrationDB].[dbo].[H_Organisatie_Eenheid] rh
+--      on rh.h_Organisatie_EenheidHashkey = resultaat.h_Organisatie_EenheidHashkey
+--inner join [TestPresentationDB].[dbo].[Dim_Organisatie_Eenheid] do
+--      on do.code = resultaat.code 
 
 
 -- zet fk weer aan na laden
