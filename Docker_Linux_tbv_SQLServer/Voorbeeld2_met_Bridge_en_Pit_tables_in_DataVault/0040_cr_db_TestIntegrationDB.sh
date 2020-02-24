fa5fb8a2fc0d90e57aee408bb437442163b4d6b7
cx1964 <cx1964@mail.com>
2020-02-24 13:10:32 +0100

    aanpassing directorynaam i n sh scripts

--- Docker_Linux_tbv_SQLServer/Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0040_cr_db_TestIntegrationDB.sh
+++ Docker_Linux_tbv_SQLServer/Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0040_cr_db_TestIntegrationDB.sh
@@ -1,4 +1,4 @@
 # https://docs.microsoft.com/en-us/sql/linux/quickstart-install-connect-docker?view=sql-server-ver15&pivots=cs1-bash
 
 # create SQL server database owners for DWH databases
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Welkom01" -i "../../cr_DBs_scripts_for_Linux/Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0040_cr_db_TestIntegration2DB.sql" -o "./0040_cr_db_TestIntegration2DB.log"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Welkom01" -i "../../cr_DBs_scripts_for_Linux/Vbld2_Bridge_en_Pit_tables_DV/0040_cr_db_TestIntegration2DB.sql" -o "./0040_cr_db_TestIntegration2DB.log"
--- Docker_Linux_tbv_SQLServer/Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0300_create_hub_tables.sh
+++ Docker_Linux_tbv_SQLServer/Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0300_create_hub_tables.sh
@@ -1,4 +1,4 @@
 # https://docs.microsoft.com/en-us/sql/linux/quickstart-install-connect-docker?view=sql-server-ver15&pivots=cs1-bash
 
 # create SQL server database owners for DWH databases
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Welkom01" -i "../../Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0300_create_hub_tables.sql" -o "./0300_create_hub_tables.log"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Welkom01" -i "../../Vbld2_Bridge_en_Pit_tables_DV/0300_create_hub_tables.sql" -o "./0300_create_hub_tables.log"
