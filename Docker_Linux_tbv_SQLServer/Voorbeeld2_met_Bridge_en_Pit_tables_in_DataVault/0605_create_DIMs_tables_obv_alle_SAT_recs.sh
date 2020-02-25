1299c54bc9efe072a352b40212e2e96288112014
cx1964 <cx1964@mail.com>
2020-02-25 08:09:38 +0100

    Nieuwe scripts tbv DIMs obv alle SAT recs

---
+++ Docker_Linux_tbv_SQLServer/Voorbeeld2_met_Bridge_en_Pit_tables_in_DataVault/0605_create_DIMs_tables_obv_alle_SAT_recs.sh
@@ -0,0 +1,4 @@
+# https://docs.microsoft.com/en-us/sql/linux/quickstart-install-connect-docker?view=sql-server-ver15&pivots=cs1-bash
+
+# create SQL server database owners for DWH databases
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Welkom01" -i "../../Vbld2_Bridge_en_Pit_tables_DV/0605_create_DIMs_tables_obv_alle_SAT_recs.sql" -o "./0605_create_DIMs_tables_obv_alle_SAT_recs.log"
