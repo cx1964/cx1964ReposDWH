8cd3352a0d4a37ef93393587414abfaf263a49b0
cx1964 <cx1964@mail.com>
2020-02-26 14:43:14 +0100

    ToDo aanvulling beschrijving v bevinding probleem

--- Vbld2_Bridge_en_Pit_tables_DV/8000_TestFact.sql
+++ Vbld2_Bridge_en_Pit_tables_DV/8000_TestFact.sql
@@ -1,6 +1,7 @@
 use [TestPresentationDB2]
 go
 
+-- geeft geen records
 SELECT   d.code
         ,d.naam 
         ,f.aantal_gepensioneerden 
@@ -12,3 +13,41 @@ inner join TestPresentationDB2.dbo.Dim_Organisatie_Eenheid_Compleet d
            and d.meta_load_end_date = f.meta_load_end_date 
            and d.meta_create_end_time = f.meta_create_end_time 
          );
+
+
+select d.H_Organisatie2HashKey
+      ,d.meta_load_date
+	  ,d.meta_create_time
+	  ,d.meta_load_end_date
+	  ,d.meta_create_end_time
+	  ,d.code
+FROM TestPresentationDB2.dbo.Dim_Organisatie_Eenheid_Compleet d
+where d.code = '46500' 
+order by 1,2,3,4,5;
+go
+
+select f.H_Organisatie2HashKey
+      ,f.meta_load_date
+	  ,f.meta_create_time
+	  ,f.meta_load_end_date
+	  ,f.meta_create_end_time
+FROM TestPresentationDB2.dbo.FACT_gepensioneerde_per_OE_Compleet f
+order by 1,2,3,4,5;
+go
+
+SELECT   d.code
+        ,d.naam 
+        ,f.aantal_gepensioneerden
+		,'toont nu wel records op join van deel van de kolommen'
+FROM TestPresentationDB2.dbo.FACT_gepensioneerde_per_OE_Compleet f
+inner join TestPresentationDB2.dbo.Dim_Organisatie_Eenheid_Compleet d
+      on (     d.H_Organisatie2HashKey = f.H_Organisatie2HashKey
+	       -- andere twee kolommen van de FK hebben nog niet correcte waarde
+	       and d.meta_load_end_date = f.meta_load_end_date 
+           and d.meta_create_end_time = f.meta_create_end_time 
+         );
+go
+
+-- de problem kolommen in PK van DIM en FK van FACT zijn:
+           --and d.meta_load_date = f.meta_load_date
+           --and d.meta_create_time  = f.meta_create_time
