eae4d4fc9a7de445c19ebc90c36167c41e33aa7c
cx1964 <cx1964@mail.com>
2020-03-04 15:37:12 +0100

    Initele script voor laden SATs obv DV 2.0

--- Vbld3_DataVault_2.0/0450_load_SATs3.sql
+++ Vbld3_DataVault_2.0/0450_load_SATs3.sql
@@ -1,20 +1,40 @@
 -- vullen sat S_Medewerker en S_Organisatie_Eenheid
 
--- Artikel over merge statement
--- https://www.sqlservertutorial.net/sql-server-basics/sql-server-merge/
 
-
-use [TestIntegrationDB2]
+use [TestIntegrationDB3]
 go
 
--- run procedure to load S_Medewerker
-exec Load_S_Medewerker2_nvrtrw
+-- load S_Medewerker3_nvrtrw
+insert into [TestIntegrationDB3].dbo.S_Medewerker3_nvrtrw
+(
+        [H_Medewerker3Hashkey]
+	   ,[hoogste_opleiding]
+	   ,[bril_dragend]
+	   ,[schoenmaat]
+	   ,[meta_record_source]
+	   ,[meta_load_date]
+	   ,[meta_create_time]
+)
+select 
+        stg.[hashkey]            as [H_Medewerker3Hashkey]
+	   ,stg.[hoogste_opleiding]
+	   ,stg.[bril_dragend]
+	   ,stg.[schoenmaat]
+	   ,stg.[meta_record_source]
+	   ,stg.[meta_load_date]
+	   ,stg.[meta_create_time]
+from TestStagingDB3.dbo.Medewerker3 stg
+-- Voeg alleen nieuwe nog niet voorkomende records toe in de SAT
+left join TestIntegrationDB3.dbo.S_Medewerker3_nvrtrw sat
+      on (    sat.H_Medewerker3Hashkey = stg.hashkey
+          and sat.meta_load_date = stg.meta_load_date
+          and sat.meta_create_time = stg.meta_create_time
+      )
 go
 
-exec Load_S_Medewerker2_vrtrw
+-- Load S_Medewerker3_vrtrw
 go
 
--- run procedure to load S_Medewerker
-exec TestIntegrationDB2.dbo.Load_S_Organisatie_Eenheid2
+-- load S_Organisatie_Eenheid3
 go
 
