48f8ff0ab47538e98f55b5629536ba43575e04fc
cx1964 <cx1964@mail.com>
2020-03-09 10:58:35 +0100

    Load DIMs special recs DV 2.0

---
+++ Vbld3_DataVault_2.0/0600_load_dims_speciale_records_traditioneel.sql
@@ -0,0 +1,36 @@
+-- Vul de DIMs met de speciale records.
+-- De speciale records onstaan, dat men alle velden van de FACTs uit verplichte velden (dus zonder NULL values)
+-- wil hebben, omdat NULL values niet goed samengaan met indexen.
+-- Daarom heeft men een speciale of meerdere speciale waarden nodig om aan te gegeven,
+-- dat iets geen waarde heeft.
+-- Aangezien de NULL value voor meerdere betekenissen wordt gebruikt, namelijk:
+--    1. onbekend
+--    2. geen waarde
+--    enz.
+--  
+-- De waarden en het aantal records voor de speciale gevallen zijn niet standaard
+-- Hierin kan men zelf keuzes maken.
+
+-- Ik gebruik de methode van oude DWH
+-- -1 = Niet gevonden
+-- -2 = Onbekend
+
+use [TestPresentationDB3]
+go
+
+-- verwijder FK tussen FACT en DIM tbv laden
+ALTER TABLE [dbo].[FACT_gepensioneerde_per_OE_Compleet] DROP CONSTRAINT [FK_FACT_gepensioneerde_per_OE_Compleet_Dim_Organisatie_Eenheid_compleet];
+GO
+
+truncate table Dim_Medewerker_Compleet;
+truncate table Dim_Organisatie_Eenheid_Compleet;
+go
+
+
+insert into Dim_Organisatie_Eenheid_Compleet (H_Organisatie2HashKey,code,naam,meta_record_source,meta_load_date,meta_create_time) values ('-1', 'Niet gevonden', NULL, 'system', convert(date, getdate()) , convert(time, getdate()));
+insert into Dim_Organisatie_Eenheid_Compleet (H_Organisatie2HashKey,code,naam,meta_record_source,meta_load_date,meta_create_time) values ('-2', 'Onbekend', NULL, 'system', convert(date, getdate()) , convert(time, getdate()));
+go  
+
+insert into Dim_Medewerker_Compleet (H_Medewerker2Hashkey,nr,voorletters,voorvoegsel,achternaam,geboortedatum,aow_datum,meta_record_source,meta_load_date,meta_create_time) values (-1,'??????',NULL,NULL,'Niet gevonden','99991231','99991231','system', convert(date, getdate()) , convert(time, getdate()));
+insert into Dim_Medewerker_Compleet (H_Medewerker2Hashkey,nr,voorletters,voorvoegsel,achternaam,geboortedatum,aow_datum,meta_record_source,meta_load_date,meta_create_time) values (-2,'??????',NULL,NULL,'Onbekend','99991231','99991231','system', convert(date, getdate()) , convert(time, getdate()));
+go  
