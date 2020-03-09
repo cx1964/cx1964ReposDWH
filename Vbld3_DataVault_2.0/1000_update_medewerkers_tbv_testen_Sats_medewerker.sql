c7e1cda1fd715e212289a00eb60ddeafbfc33cc8
cx1964 <cx1964@mail.com>
2020-03-09 15:03:30 +0100

    test records

--- Vbld3_DataVault_2.0/1000_update_medewerkers_tbv_testen_Sats_medewerker.sql
+++ Vbld3_DataVault_2.0/1000_update_medewerkers_tbv_testen_Sats_medewerker.sql
@@ -2,7 +2,11 @@ use [TestSourceDB3];
 go
 
 update TestSourceDB3.dbo.Medewerker3
-set schoenmaat = 47
+set schoenmaat = 48
 where nr = '000010';
 go
 
+update TestSourceDB3.dbo.Medewerker3
+set aow_datum = '19900701'
+where nr = '000010';
+go
