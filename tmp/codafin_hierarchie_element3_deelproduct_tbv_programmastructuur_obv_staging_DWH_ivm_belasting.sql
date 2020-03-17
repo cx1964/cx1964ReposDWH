822ccaf19b21a12949e123604af1bebcf37e9668
cx1964 <cx1964@mail.com>
2020-03-17 08:46:41 +0100

    script tbv werken op staging ivm belasting

---
+++ tmp/codafin_hierarchie_element3_deelproduct_tbv_programmastructuur_obv_staging_DWH_ivm_belasting.sql
@@ -0,0 +1,43 @@
+-- Filenaam: codafin_hierarchie_element3_deelproduct_tbv_programmastructuur_obv_staging_DWH_ivm_belasting
+-- Functie: query deelproduct hierarchie tbv programmastructuur obv staging database DWH ivm belasing bron
+
+-- use codafin
+use DWH_STG;
+go
+
+
+-- check ook de queries in SSRS rapporten
+-- Referentie DWH sem.SV_KOSTENPLAATS
+
+-- elm=2 kan ook crediteuren en debiteuren zijn
+
+select     element.code,
+           element.name,
+           grplist.cmpcode,
+           grplist.code,
+           grplist.grpcode,
+           CAST(himlist.code AS VARCHAR(20)) AS hierarchie,
+           himlist.l1name,
+           himlist.l1hdrtxt,
+           himlist.l1order,
+           himlist.l2name,
+           himlist.l2hdrtxt,
+           himlist.l2order,
+           himlist.leafname,
+           himlist.leafhdrtxt,
+           himlist.leaforder
+from codafin.dbo.oas_element element
+left join codafin.dbo.oas_grplist grplist
+      on (
+          element.cmpcode  = grplist.cmpcode
+      and element.code     = grplist.code
+      and element.elmlevel = grplist.elmlevel
+      )
+inner join codafin.dbo.oas_himlist himlist
+      on (
+          himlist.cmpcode = grplist.cmpcode
+      and himlist.grpcode = grplist.grpcode     
+      )     
+where 1=1
+  and element.elmlevel = 3
+go
