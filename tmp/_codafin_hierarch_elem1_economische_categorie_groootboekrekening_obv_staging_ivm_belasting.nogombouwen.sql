c324bfb2d504d9acc397a8e81f098b66361b3bda
cx1964 <cx1964@gmail.com>
2020-03-24 15:58:02 +0100

    1e concept elem1 hierarchie

--- tmp/_codafin_hierarch_elem1_economische_categorie_groootboekrekening_obv_staging_ivm_belasting.nogombouwen.sql
+++ tmp/_codafin_hierarch_elem1_economische_categorie_groootboekrekening_obv_staging_ivm_belasting.nogombouwen.sql
@@ -1,12 +1,14 @@
--- Filenaam: codafin_hierarch_elem2_kostenplts_obv_staging_ivm_belasting.sql
--- Functie: query kostenplaats hierarchie
+-- Filenaam: _codafin_hierarch_elem1_economische_categorie_groootboekrekening_obv_staging_ivm_belasting.nogombouwen.sql
+-- Functie: query economische categorie (grootboekrekening) hierarchie
 --          query omgebouwd van bron op staging database DWH zodat bron net wordt belast
 --          Deze query gebrukt staging DWH
---          output vergeleken met ssrs rapport CI40947 "CODA Element 2 organisatiestructuur_2019"
+--          output vergeleken met ssrs rapport CI40942 "CODA Element 1 Hierarchie economische categorie"
 
 -- use codafin
 use DWH_STG;
 go
+
+-- ToDo anders sortren om te vergelijk met rapport
 
 select  
         himlist.code as [hierarchiecode] 
@@ -41,8 +43,8 @@ inner join dwh_stg.codafin12.oas_himlist himlist
       and himlist.grpcode = grplist.grpcode     
       )     
 where 1=1
-  and element.elmlevel = 2 -- kostenplaats
-  and himlist.code = 'PZHORG-D' -- bepaalt welke kostenplaats hierarchie wordt gebruikt
+  and element.elmlevel = 1 -- economische catgeorie (grootboekrekening)
+  and himlist.code = 'PZHECOCAT' -- bepaalt welke kostenplaats hierarchie wordt gebruikt
 
   -- tbv test !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   -- and himlist.l2name = 'A2107'     -- afdeingscode
@@ -55,3 +57,37 @@ order by
         ,element.code;
 go
 
+/*
+select  distinct
+        himlist.code as [hierarchiecode] 
+       ,case
+          when element.endyear = '0' 
+          then 'ja'
+          else 'nee'
+        end as geldig --  klopt dit ??????????????????
+-- from codafin.dbo.oas_element element
+from dwh_stg.codafin12.oas_element element
+-- left join codafin.dbo.oas_grplist grplist
+inner join dwh_stg.codafin12.oas_grplist grplist
+      on (
+          element.cmpcode  = grplist.cmpcode
+      and element.code     = grplist.code
+      and element.elmlevel = grplist.elmlevel
+      )
+--inner join codafin.dbo.oas_himlist himlist
+inner join dwh_stg.codafin12.oas_himlist himlist
+      on (
+          himlist.cmpcode = grplist.cmpcode
+      and himlist.grpcode = grplist.grpcode     
+      )     
+where 1=1
+  and element.elmlevel = 1 -- economische catgeorie (grootboekrekening)
+--  and himlist.code = 'PZHORG-D' -- bepaalt welke kostenplaats hierarchie wordt gebruikt
+
+  -- tbv test !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  -- and himlist.l2name = 'A2107'     -- afdeingscode
+
+order by 
+         1;
+go
+*/
