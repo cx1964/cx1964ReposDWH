5bacca54904733fca80a73af142479b4d45b3cdb
cx1964 <cx1964@gmail.com>
2020-03-25 16:02:20 +0100

    tussen versie

--- tmp/codafin_hierarch_elem3_deelprod_tbv_progstruct_obv_staging_ivm_belasting.NogTeVeelRecords.sql
+++ tmp/codafin_hierarch_elem3_deelprod_tbv_progstruct_obv_staging_ivm_belasting.NogTeVeelRecords.sql
@@ -2,21 +2,18 @@
 -- Functie: query deelproduct hierarchie tbv programmastructuur 
 --          query omgebouwd van bron op staging database DWH zodat bron niet wordt belast
 --          Deze query gebrukt staging DWH
+-- Referenties: Codalive SSRS rapport Informatieportaal productie CI40826 "CODA Element 3 programmastructuur 2020-2024" zoals op 20200325 op informatieportaal
+--              Codalive SSRS rapport Informatieportaal productie CI40826 "0078b Element 3 programmastructuur 2016-2019" 
+--              Query van Semantische view uit Semlaag DWH: sem.SV_DEELPRODUCT
+-- Opmerking: Deze query geeft voor gegeven test situatie (himlist.l3name = '9-1-1')
+--            evenveel records op voor zelfde testcase als in SSRS rapport 
+--            CI40826 "CODA Element 3 programmastructuur 2020-2024".rdl en
+--            CI40826 "0078b Element 3 programmastructuur 2016-2019".rdl
 
 -- use codafin
 use DWH_STG;
 go
 
--- ToDo
-
--- Nog vergelijken met:
--- 1. Codalive SSRS rapport Informatieportaal productie CI40826 "CODA Element 3 programmastructuur 2020-2024"
-
--- Referentie DWH sem.SV_DEELPRODUCT
-
--- nog afwijkend aantal records !!!
-
--- weg halen test condities !!!!!!!!!
 select  
         himlist.code as [hierarchiecode] 
        ,himlist.l1name as [programma_code_alias_ambitie_code] 
@@ -59,14 +56,24 @@ inner join dwh_stg.codafin12.oas_himlist himlist
 where 1=1
   and element.elmlevel = 3 -- deelproduct
   and grplist.elmlevel = 3 -- extra conditie uit rapport  CI40826 0078b Element 3 programmastructuur 2016-2019.rdl
-  --  and element.cmpcode  = 'PZH'
+  and element.cmpcode  = 'PZH'
   and himlist.code     = 'PZHPROG2024' -- bepaalt welke programmabegroting voor welke collegeperiode
-  --  and himlist.code     = 'PZHPROG1620' -- tbv test 
-
+  and
+       -- substitutie variabelen tbv
+       -- geldigheidscheck: geldig = 'ja'
+       ( case
+          when year(getdate()) * 10000 + month(getdate())
+            between 
+              --(case when Prog.startyear &gt; 0 then Prog.startyear else 1900 end * 10000 + Prog.startperiod)
+              (case when element.startyear > 0 then element.startyear else 1900 end * 10000 + element.startperiod)
+              and
+              --(case when Prog.endyear &gt; 0 then Prog.endyear else 9999 end * 10000 + Prog.endperiod)
+              (case when element.endyear > 0 then element.endyear else 9999 end * 10000 + element.endperiod)
+          then ('ja')
+          else ('nee')
+        end ) = 'ja'
   -- tbv test !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  --and himlist.l1name = '9'     -- ambitiecode
-  and himlist.l3name = '9-1-1' -- beleidsprestatiecode
-
+  -- and himlist.l3name = '9-1-1' -- beleidsprestatiecode
 order by himlist.l1name
         ,himlist.l2name
         ,himlist.l3name
@@ -75,6 +82,11 @@ order by himlist.l1name
 go
 
 
+-- ****************************************************************
+-- *** Onderstaande queries alleen tbv testen bovenstaande query --
+-- ****************************************************************
+
+/***
 -- query uit SSRS rapport "0078b Element 3 programmastructuur 2016-2019.rdl"
 select
   Prog.l1name as ProgrammaCode,
@@ -110,8 +122,7 @@ select
       (case when endyear > 0 then endyear else 9999 end * 10000 + endperiod)
     then ('ja')
     else ('nee')
-  end
-  as Geldig
+  end as Geldig
 from
 (select 
   oas_element.code  as  code,
@@ -148,5 +159,97 @@ where
   oas_himlist.code     = 'PZHPROG2024' and
   -- tbv test
   oas_himlist.l3name = '9-1-1' -- beleidsprestatiecode
-) as Prog;
+) as Prog
+where 1=1
+  and (  -- Geldigheidscheck: alleen Geldig='ja'
+         case
+           when year(getdate()) * 10000 + month(getdate())
+             between 
+               --(case when Prog.startyear &gt; 0 then Prog.startyear else 1900 end * 10000 + Prog.startperiod)
+               (case when startyear > 0 then startyear else 1900 end * 10000 + startperiod)
+               and
+               --(case when Prog.endyear &gt; 0 then Prog.endyear else 9999 end * 10000 + Prog.endperiod)
+               (case when endyear > 0 then endyear else 9999 end * 10000 + endperiod)
+             then ('ja')
+             else ('nee')
+         end ) ='ja';
 go
+
+
+-- query Greisy uit uit SSRS rapport CI40826 "CODA Element 3 programmastructuur 2020-2024"
+-- Declare @Geldig as NVARCHAR(10) = 'Ja'; -- Ja , Nee of * 
+select * 
+from 
+(select
+  Prog.l1name as ProgrammaCode,
+  Prog.l1hdrtxt as ProgrammaNaam,
+  Prog.l2name as DoelCode,
+  Prog.l2hdrtxt as DoelNaam,
+  Prog.l3name as TaakCode,
+  Prog.l3order as Taakorder,
+  Prog.l3hdrtxt as TaakNaam,
+  Prog.leafname as ProductCode,
+  Prog.leafhdrtxt as ProductNaam,
+  Prog.code as DeelproductCode,
+  Prog.name as DeelproductNaam,
+  case
+    when year(getdate()) * 10000 + month(getdate())
+    between 
+    (case when Prog.startyear > 0 then Prog.startyear else 1900 end * 10000 + Prog.startperiod)
+    and
+    (case when Prog.endyear > 0 then Prog.endyear else 9999 end * 10000 + Prog.endperiod)
+    then ('ja')
+    else ('nee')
+  end
+  as Geldig
+
+from
+(select 
+  oas_element.code  as  code,
+  oas_element.name  as  name,
+  oas_element.sname  as  sname,
+  oas_grplist.grpcode  as  grpcode,
+  oas_himlist.l1name  as  l1name,
+  oas_himlist.l1hdrtxt  as  l1hdrtxt,
+  oas_himlist.l2name  as  l2name,
+  oas_himlist.l2hdrtxt  as  l2hdrtxt,
+  oas_himlist.l3name  as  l3name,
+  oas_himlist.l3order  as  l3order,
+  oas_himlist.l3hdrtxt  as  l3hdrtxt,
+  oas_himlist.leafname  as  leafname,
+  oas_himlist.leafhdrtxt  as  leafhdrtxt,
+  oas_element.endyear as endyear, 
+  oas_element.endperiod as endperiod, 
+  oas_element.startperiod as startperiod, 
+  oas_element.startyear as startyear
+from
+  /*
+  dbo.oas_element oas_element,
+  dbo.oas_grplist oas_grplist,
+  dbo.oas_himlist oas_himlist
+  */
+  dwh_stg.codafin12.oas_element oas_element,
+  dwh_stg.codafin12.oas_grplist oas_grplist,
+  dwh_stg.codafin12.oas_himlist oas_himlist 
+where
+  oas_element.elmlevel = 3 and
+  oas_grplist.elmlevel = 3 and
+  oas_grplist.cmpcode  = oas_himlist.cmpcode and
+  oas_grplist.grpcode  = oas_himlist.grpcode and
+  oas_element.code     = oas_grplist.code and
+  oas_element.elmlevel = oas_grplist.elmlevel and
+  oas_element.cmpcode  = oas_grplist.cmpcode and
+  oas_element.cmpcode  = 'PZH' and
+  oas_himlist.code     = 'PZHPROG2024'
+) as Prog
+
+) R
+
+where  (R.Geldig = '*') or (R.Geldig = 'ja')
+       -- mijn test situtaie
+       and R.TaakCode = '9-1-1' -- beleidsprestatiecode
+order by
+  DeelproductCode;
+go 
+
+***/
