141b44288ce1f39e0ab8bf82e00b2962e1976a3a
cx1964 <cx1964@gmail.com>
2020-03-25 09:33:50 +0100

    sortering nu identiek als in ssrs rapport

--- tmp/codafin_hierarch_elem1_economische_categorie_grootboekrekening_obv_staging_ivm_belasting.sql
+++ tmp/codafin_hierarch_elem1_economische_categorie_grootboekrekening_obv_staging_ivm_belasting.sql
@@ -1,14 +1,13 @@
--- Filenaam: _codafin_hierarch_elem1_economische_categorie_groootboekrekening_obv_staging_ivm_belasting.nogombouwen.sql
--- Functie: query economische categorie (grootboekrekening) hierarchie
---          query omgebouwd van bron op staging database DWH zodat bron net wordt belast
---          Deze query gebrukt staging DWH
---          output vergeleken met ssrs rapport CI40942 "CODA Element 1 Hierarchie economische categorie"
+-- Filenaam: codafin_hierarch_elem1_economische_categorie_grootboekrekening_obv_staging_ivm_belasting.sql
+-- Functie:  query economische categorie (grootboekrekening) hierarchie
+--           Deze query gebrukt staging DWH
+--           output vergeleken met ssrs rapport CI40942 "CODA Element 1 Hierarchie economische categorie"
 
 -- use codafin
 use DWH_STG;
 go
 
--- ToDo anders sortren om te vergelijk met rapport
+-- ToDo nog testen op volledigheid van de gegevens
 
 select  
         himlist.code as [hierarchiecode] 
@@ -59,48 +58,8 @@ where 1=1
   -- and element.code = '00101'     -- grootboekrekeningnummer
 
 order by 
-         himlist.l1name
---       ,himlist.l2name
---       ,himlist.l3name
---       ,himlist.leafname
-        ,element.code;
+         element.code;
 go
 
 -- select *
 -- from dwh_stg.codafin12.oas_group as grp -- group is reserverd word !
-
-
-/*
-select  distinct
-        himlist.code as [hierarchiecode] 
-       ,case
-          when element.endyear = '0' 
-          then 'ja'
-          else 'nee'
-        end as geldig --  klopt dit ??????????????????
--- from codafin.dbo.oas_element element
-from dwh_stg.codafin12.oas_element element
--- left join codafin.dbo.oas_grplist grplist
-inner join dwh_stg.codafin12.oas_grplist grplist
-      on (
-          element.cmpcode  = grplist.cmpcode
-      and element.code     = grplist.code
-      and element.elmlevel = grplist.elmlevel
-      )
---inner join codafin.dbo.oas_himlist himlist
-inner join dwh_stg.codafin12.oas_himlist himlist
-      on (
-          himlist.cmpcode = grplist.cmpcode
-      and himlist.grpcode = grplist.grpcode     
-      )     
-where 1=1
-  and element.elmlevel = 1 -- economische catgeorie (grootboekrekening)
---  and himlist.code = 'PZHORG-D' -- bepaalt welke kostenplaats hierarchie wordt gebruikt
-
-  -- tbv test !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  -- and himlist.l2name = 'A2107'     -- afdeingscode
-
-order by 
-         1;
-go
-*/
