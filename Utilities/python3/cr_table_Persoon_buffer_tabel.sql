855b2e7fecefdf7c8fc67cf6c4811a2f8f3f117b
cx1964 <cx1964@mail.com>
2020-03-29 14:33:30 +0200

    Werkend voorbeeld python odbc script

---
+++ Utilities/python3/cr_table_Persoon_buffer_tabel.sql
@@ -0,0 +1,24 @@
+USE [TestSourceDB3]
+GO
+
+/****** Object:  Table [dbo].[Persoon]    Script Date: 29-3-2020 14:13:42 ******/
+DROP TABLE [dbo].[Persoon]
+GO
+
+/****** Object:  Table [dbo].[Persoon]    Script Date: 29-3-2020 14:13:42 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Persoon](
+	[id] [varchar](3) NOT NULL,
+	[voorletters] [varchar](15) NOT NULL,
+	[achternaam] [varchar](30) NOT NULL,
+	[geboortedatum] [varchar](10) NOT NULL
+) ON [PRIMARY]
+GO
+
+
+
--- Utilities/python3/insert_data_py3.py
+++ Utilities/python3/insert_data_py3.py
@@ -1,6 +1,7 @@
 # Filenaam: insert_data_py3.py
-# Functie:  python3 script om data via odbc in SQL server database te importeren.
+# Functie:  werkend python3 script om data via odbc in SQL server database te importeren.
 # Gebruikte python versie: 3.8.2
+# Referenties: https://www.got-it.ai/solutions/sqlquerychat/sql-help/data-manipulation/read-and-write-data-to-and-from-sql-server-using-pandas-library-in-python-querychat/
 # Opmerking:
 #             Requirements Windows:
 #             - Zorg dat mbv het script install_python_env.cmd de environment is geinstalleerd
@@ -13,13 +14,22 @@
 #               - localhost_db_TestPresentationDB3
 #               Draai dit script vanuit Microsoft Visual Code mbv de rechter muis optie "Run Python file in terminal"
 #               Zorg dat in Visual code de python interpreter wordt gebruikt uit virtual env env_python3_DWH
-# Referenties: https://www.got-it.ai/solutions/sqlquerychat/sql-help/data-manipulation/read-and-write-data-to-and-from-sql-server-using-pandas-library-in-python-querychat/
+#
+#  Letop: Dit voorbeeld werkt zowel met de textfiles
+#         testtextfile.csv en testtextfile_quoted.csv.
+#         In beide gevallen verdwijnen nog de voorloop nullen van het veld id.
+#         Ook worden alle velden als varchar velden ingelezen in de database.
+#         Ook de datum velden.
+
+# *** ToDo 
+#  1. uitzoeken hoe data ook ingelezen kan worden in andere datatype als varchar (zoals date)
+#  2. nog uitproberen de pandas _to_sql commando
 
 import pyodbc
 import pandas as pd
 
 #werkt 
-data = pd.read_csv("testtextfile.csv", skiprows=6, sep=';', skip_blank_lines=True)
+data = pd.read_csv("testtextfile_quoted.csv", skiprows=6, sep=';', skip_blank_lines=True)
 
 # zorg dat de kolommen zijn ge-quote en kolommen gescheiden zijn obv ;
 #data = pd.read_csv("C:\tmp\weg\Data\query_data_brontabel_oas_himlist.data", skiprows=6, sep=';', skip_blank_lines=True)
@@ -36,9 +46,24 @@ print(data.head(5))
 #         pyodbc_voorbeeld_naar_sqlserver_py3.py
 sql_conn = pyodbc.connect('DSN=localhost_db_TestSourceDB3;UID=sa;PWD=Welkom01')
 cursor = sql_conn.cursor() 
-for index, row in df.iterrows(): 
+
+# Maak de tabel leeg, voor het geval al data bevat
+cursor.execute("truncate table Persoon")
+
+# Lees de data uit de textfile in de database 
+for index, row in data.iterrows(): 
   print(row) 
-  cursor.execute("INSERT INTO Persoon([Id],[Voorletters],[Achternaam],[Geboortedatum]) values(?,?,?,?)", row['Id'], row['Voorletters'], row['Achternaam'], row['Geboortedatum']) 
+  cursor.execute("INSERT INTO Persoon(\
+                     [Id] \
+                    ,[Voorletters] \
+                    ,[Achternaam] \
+                    ,[Geboortedatum] \
+                 )\
+                 values(?,?,?,?)",
+                    row['Id']
+                   ,row['Voorletters']
+                   ,row['Achternaam']
+                   ,row['Geboortedatum']) 
 sql_conn.commit() 
 cursor.close() 
 sql_conn.close() 
