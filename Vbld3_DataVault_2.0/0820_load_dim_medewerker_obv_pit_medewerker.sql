486d210fb7ead9b643512fa44bfe902f4d022c72
cx1964 <cx1964@mail.com>
2020-05-06 13:59:25 +0200

    tussen resultaat mbt change_hashkey tbv pit

--- Vbld3_DataVault_2.0/0820_load_dim_medewerker_obv_pit_medewerker.sql
+++ Vbld3_DataVault_2.0/0820_load_dim_medewerker_obv_pit_medewerker.sql
@@ -4,9 +4,11 @@
 use TestIntegrationDB3;
 go
 
-
 -- ToDo
 -- Oplossen probleem dat Dim records niet uniek zijn
+
+-- Probleem 6-5-2020
+-- Gaat iets mis met aan joinen sat vrtw aan pit tabel ?????????????????????????????
 
 insert into [TestPresentationDB3].[dbo].[Dim_Medewerker_Pit](
   [H_Medewerker3Hashkey]
@@ -37,16 +39,18 @@ select
 	   ,hm.nr
 
         -- uit Sat S_Medewerker3_nvrtrw
-	   ,s_m_nvrtrw.meta_load_date    as meta_load_date_nvrtrw -- extra toegevoegd
-	   ,s_m_nvrtrw.meta_create_time  as meta_create_time_nvrtrw -- extra toegevoegd
+       ,s_m_nvrtrw.meta_change_hashkey as nvrtrw_change_hashkey -- extra toegevoegd
+	   ,s_m_nvrtrw.meta_load_date      as meta_load_date_nvrtrw -- extra toegevoegd
+	   ,s_m_nvrtrw.meta_create_time    as meta_create_time_nvrtrw -- extra toegevoegd
 
        ,s_m_nvrtrw.[hoogste_opleiding]
        ,s_m_nvrtrw.[bril_dragend]
        ,s_m_nvrtrw.[schoenmaat]
 
         -- uit Sat S_Medewerker3_vrtrw
-	   ,s_m_vrtrw.meta_load_date    as meta_load_date_vrtrw  -- extra toegevoegd
-	   ,s_m_vrtrw.meta_create_time  as meta_create_time_vrtrw -- extra toegevoegd
+       ,s_m_vrtrw.meta_change_hashkey as vrtrw_change_hashkey -- extra toegevoegd
+	   ,s_m_vrtrw.meta_load_date       as meta_load_date_vrtrw  -- extra toegevoegd
+	   ,s_m_vrtrw.meta_create_time     as meta_create_time_vrtrw -- extra toegevoegd
 		
        ,s_m_vrtrw.[voorletters]
        ,s_m_vrtrw.[voorvoegsel]
@@ -59,12 +63,14 @@ select
 from [TestIntegrationDB3].dbo.Pit_Medewerker pm
 inner join dbo.H_Medewerker3 hm
       on     hm.H_Medewerker3Hashkey = pm.H_Medewerker3Hashkey
-inner join dbo.S_Medewerker3_nvrtrw s_m_nvrtrw
+left join dbo.S_Medewerker3_nvrtrw s_m_nvrtrw
       on     s_m_nvrtrw.H_Medewerker3Hashkey = pm.H_Medewerker3Hashkey
-	     and s_m_nvrtrw.meta_load_date       = pm.nvrtrw_load_date
-		 and s_m_nvrtrw.meta_create_time     = pm.nvrtrw_create_time
-inner join dbo.S_Medewerker3_vrtrw s_m_vrtrw
+	     and s_m_nvrtrw.meta_change_hashkey  = pm.nvrtrw_change_Hashkey
+	  --   and s_m_nvrtrw.meta_load_date       = pm.nvrtrw_load_date
+		 --and s_m_nvrtrw.meta_create_time     = pm.nvrtrw_create_time
+left join dbo.S_Medewerker3_vrtrw s_m_vrtrw
       on     s_m_vrtrw.H_Medewerker3Hashkey = pm.H_Medewerker3Hashkey
-	     and s_m_vrtrw.meta_load_date       = pm.vrtrw_load_date
-		 and s_m_vrtrw.meta_create_time     = pm.vrtrw_create_time
+	     and s_m_vrtrw.meta_change_hashkey  = pm.vrtrw_change_Hashkey
+	  --   and s_m_vrtrw.meta_load_date       = pm.vrtrw_load_date
+		 --and s_m_vrtrw.meta_create_time     = pm.vrtrw_create_time
 order by 1,2,3
