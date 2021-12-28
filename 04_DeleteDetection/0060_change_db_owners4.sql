98b5109bb1be720992f0f2046c0a81aeb679f178
cx1964 <cx1964@mail.com>
2021-12-28 19:56:01 +0100

    initiele create db object scripts

--- 04_DeleteDetection/0060_change_db_owners4.sql
+++ 04_DeleteDetection/0060_change_db_owners4.sql
@@ -1,17 +1,17 @@
-/* Filenaam   : 0060_change_db_owners3.sql
- * Functie    : SQL-T SQL2016
+/* Filenaam   : 0060_change_db_owners4.sql
+ * Functie    : SQL-T SQL2019
  *              script tbv toewijzen van de applicatie database user
- *              als owner van de database tbv voorbeeld3
+ *              als owner van de database tbv voorbeeld4
  * Opmerkingen: Dit script moet gedraaid worden nadat de lege database is gemaakt
  *
  * --------- history ---------------------
- * v.0.0.0   2020-03-04   C.X. la Fontaine
+ * v.0.0.0   2021-12-28   C.X. la Fontaine
  */
 
 
 -- *** Begin Change owner TestSourceDB DB ***
 /* Selecteer de nieuwe lege applicatie database */ 
-use TestSourceDB3
+use TestSourceDB4
 go
 
 /* Maak de Applicatie database user eigenaar van de database */
@@ -22,7 +22,7 @@ go
 
 -- *** Begin Change owner TestStagingDB DB ***
 /* Selecteer de nieuwe lege applicatie database */ 
-use TestStagingDB3
+use TestStagingDB4
 go
 
 /* Maak de Applicatie database user eigenaar van de database */
@@ -33,7 +33,7 @@ go
 
 -- *** Begin Change owner TestIntegrationDB DB ***
 /* Selecteer de nieuwe lege applicatie database */ 
-use TestIntegrationDB3
+use TestIntegrationDB4
 go
 
 /* Maak de Applicatie database user eigenaar van de database */
@@ -43,7 +43,7 @@ go
 
 -- *** Begin Change owner TestPresentationDB DB ***
 /* Selecteer de nieuwe lege applicatie database */ 
-use TestPresentationDB3
+use TestPresentationDB4
 go
 
 /* Maak de Applicatie database user eigenaar van de database */
