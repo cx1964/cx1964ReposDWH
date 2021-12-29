20de91781e1b9f21c0a73fcbef872b4ef7fadf38
cx1964 <cx1964@mail.com>
2021-12-29 14:53:30 +0100

    werkend DeleteDetection script

---
+++ 04_DeleteDetection/0610_load_staging_tables4.sql
@@ -0,0 +1,90 @@
+-- Letop dit script moet een copy zijn van 0520_load_staging_tables4.sql
+
+use [TestStagingDB4]
+go
+
+truncate table [dbo].[Medewerker4];
+go
+truncate table [dbo].[Organisatie_Eenheid4];
+go
+
+
+insert into Organisatie_Eenheid4
+( 
+  -- gegenereerde Hashkey obv business key tbv de Hub
+    hashkey
+  -- surrogate key 
+  ,[id]
+  -- Busness key
+  ,[code]
+  -- Properties
+  ,[naam]
+  -- Meta gegevens
+  ,[meta_record_source]
+  ,[meta_load_date]
+  ,[meta_create_time]
+)
+select 
+      -- gegenereerde Hashkey obv business key tbv de Hub
+      -- SHA2_256 geeft 32 bits key
+       HASHBYTES('SHA2_256', [code]) as hashkey
+      -- surrogate key 
+      ,[id]
+      -- Busness key
+      ,[code]
+      -- Properties
+      ,[naam]
+      -- Meta gegevens
+      ,'fictief' as [meta_record_source]
+	    ,convert(date, getdate()) as [meta_load_date]
+	    ,convert(time, getdate()) as [meta_create_time]
+from [TestSourceDB4].[dbo].[Organisatie_Eenheid4];
+go
+
+
+insert into [Medewerker4]
+(
+   -- gegenereerde Hashkey obv business key tbv de Hub
+   hashkey
+  -- surrogate key 
+  ,[id]
+  -- Busness key
+  ,[nr]
+  -- Properties
+  ,[voorletters]
+  ,[voorvoegsel]
+  ,[achternaam]
+  ,[geboortedatum]
+  ,[aow_datum]
+  ,[werkt_voor_org_eenheid]
+  ,[hoogste_opleiding]
+  ,[bril_dragend]
+  ,[schoenmaat]
+  -- Meta gegevens
+  ,[meta_record_source]
+  ,[meta_load_date]
+  ,[meta_create_time]
+)
+select 
+      -- gegenereerde Hashkey obv business key tbv de Hub
+      -- SHA2_256 geeft 32 bits key
+       HASHBYTES('SHA2_256', [nr]) as hashkey
+	    ,[id]
+      -- Business key
+      ,[nr]
+      -- Properties
+      ,[voorletters]
+      ,[voorvoegsel]
+      ,[achternaam]
+      ,[geboortedatum]
+      ,[aow_datum]
+      ,[werkt_voor_org_eenheid]
+      ,[hoogste_opleiding]
+      ,[bril_dragend]
+      ,[schoenmaat]
+      -- Meta gegevens
+      ,'fictief' as [meta_record_source]
+	    ,convert(date, getdate()) as [meta_load_date]
+	    ,convert(time, getdate()) as [meta_create_time]
+from [TestSourceDB4].[dbo].[Medewerker4];
+go
--- 04_DeleteDetection/0700_process_DeleteDetion_inb_HUBs4.sql
+++ 04_DeleteDetection/0700_process_DeleteDetion_inb_HUBs4.sql
@@ -9,7 +9,9 @@ go
 -- verwerk H_Organisatie_Eenheid4
 UPDATE [TestIntegrationDB4].DBO.H_Organisatie_Eenheid4
 SET meta_IsDeletedInSource = 'TRUE'
-WHERE CODE in (
+WHERE code /* business key */  in (
+    -- Alle hub records die een business key in de hub hebben
+    -- maar niet in de bron/staging
     SELECT code
     FROM [TestIntegrationDB4].[DBO].[H_Organisatie_Eenheid4]
     WHERE 1=1
@@ -22,13 +24,26 @@ WHERE CODE in (
 
 
 -- verwerk H_Medewerker4
-
+UPDATE [TestIntegrationDB4].DBO.H_Medewerker4
+SET meta_IsDeletedInSource = 'TRUE'
+WHERE nr /* business key */ in (
+    -- Alle hub records die een business key in de hub hebben
+    -- maar niet in de bron/staging
+    SELECT nr
+    FROM [TestIntegrationDB4].[DBO].[H_Medewerker4]
+    WHERE 1=1
+      and meta_IsDeletedInSource = 'FALSE'
+    EXCEPT
+    SELECT nr
+    FROM [TestStagingDB4].[DBO].[Medewerker4]
+    WHERE 1=1
+)    
 
 
 
 -- Debug info
-SELECT*
-FROM dbo.H_Medewerker4 
+SELECT *
+FROM [TestIntegrationDB4].[DBO].[H_Medewerker4] 
 WHERE 1=1
   AND meta_IsDeletedInSource = 'TRUE'
 
