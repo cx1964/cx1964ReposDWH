926b5c6e6bd155b531a215638650e950ae93e18a
cx1964 <cx1964@mail.com>
2021-12-29 14:22:41 +0100

    update

--- 04_DeleteDetection/0600_create_test_data_DeleteDeteion.sql
+++ 04_DeleteDetection/0600_create_test_data_DeleteDeteion.sql
@@ -5,17 +5,22 @@ use [TestSourceDB4]
 go
 
 -- Delete Organisatie_Eenheid4 Record from source
+BEGIN TRANSACTION
 DELETE
 FROM [TestSourceDB4].DBO.Organisatie_Eenheid4
 WHERE 1=1
   and code = '46300'
-
+COMMIT
+go
 
 -- Delete H_Medewerker4 Record from source
+BEGIN TRANSACTION
 DELETE
 FROM [TestSourceDB4].DBO.Medewerker4
 WHERE 1=1
   and nr = '000020'
+COMMIT
+go
 
 -- Debug info
 select *
